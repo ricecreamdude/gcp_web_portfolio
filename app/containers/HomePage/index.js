d7d65abdf5869c59e1e17318b366092fd9e690b4
Joshua Ho <ho.joshua4@gmail.com>
2019-03-16 15:10:34 -0700

    Moving header components to index.html

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,26 +10,12 @@
  */
 
 import React from 'react';
-import Helmet from 'react-helmet';
 import Navigation from 'components/Navigation';
 
-export default class HomePage extends React.PureComponent {
-  render() {
-    return (
-      <div className='Application'>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>joshuaho.me</title>
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-            crossorigin="anonymous"
-          />
-        </Helmet>
-        <Navigation text="Hello I am a header" />
-      </div>
-      
-    );
-  }
+export default function HomePage(){
+  return (
+    <div className='Application'>
+      <Navigation text="Hello I am a header" />
+    </div>
+  );
 }
