a4b285329e36c40d34773a4a8a9d1ae8d58feda4
Joshua Ho <ho.joshua4@gmail.com>
2019-03-16 14:13:04 -0700

    Second Commit

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,15 +11,17 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import Header from 'components/Header';
+
 import messages from './messages';
+
+
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <Header text="Hello I am a header" />
     );
   }
 }
